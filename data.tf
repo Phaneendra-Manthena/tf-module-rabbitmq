6a8117d75598ce7fd0ddd5bd937254bb05ec6d24
phaneendra <phani.manthena27@gmail.com>
2023-07-29 12:07:49 +0530

    updating ./tf-module-rabbitmq

---
+++ data.tf
@@ -0,0 +1,11 @@
+data "aws_ssm_parameter" "DB_ADMIN_USER" {
+  name = "${var.env}.rabbitmq.USER"
+}
+
+data "aws_ssm_parameter" "DB_ADMIN_PASS" {
+  name = "${var.env}.rabbitmq.PASS"
+}
+
+data "aws_kms_key" "key" {
+  key_id = "alias/roboshop"
+}
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
+variable "engine_version" {}
+variable "engine_type" {}
+variable "host_instance_type" {}
+variable "deployment_mode" {}
+variable "subnet_ids" {}
