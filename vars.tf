cf8720348d16ef98048e872f1c34cc17197ceccb
phaneendra <phani.manthena27@gmail.com>
2023-08-01 10:32:36 +0530

    updating ./tf-module-rabbitmq

--- vars.tf
+++ vars.tf
@@ -1,10 +1,10 @@
 variable "env" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
-variable "engine_version" {}
-variable "engine_type" {}
-variable "host_instance_type" {}
-variable "deployment_mode" {}
+#variable "engine_version" {}
+#variable "engine_type" {}
+#variable "host_instance_type" {}
+#variable "deployment_mode" {}
 variable "subnet_ids" {}
 variable "component" {
   default = "rabbitmq"
